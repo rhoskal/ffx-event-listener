7d718475dfa4f0c0eb12041471130f958aca16e9
Hans Hoffman <hans@flatfile.io>
2023-03-08 15:33:46 -0700

    fix local dev SSL

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import elmPlugin from "vite-plugin-elm";
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [basicSsl(), elmPlugin()],
 });
